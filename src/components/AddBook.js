dc8a93dc4e86fdf58f6f0babe93f6f213b268acc
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-04-09 16:39:49 +0100

    Add styles

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -25,6 +25,13 @@ const AddBook = () => {
     });
   };
 
+  const catagoryChange = (e) => {
+    setForm({
+      ...form,
+      category: e.target.value,
+    });
+  };
+
   const dispatch = useDispatch();
 
   const submitBook = (e) => {
@@ -42,9 +49,19 @@ const AddBook = () => {
 
   return (
     <div className="add_book">
+      <h2 className="header">Add new book</h2>
       <form onSubmit={(e) => submitBook(e)}>
         <input className="author" placeholder="Author name" value={form.author} onChange={(e) => authorChange(e)} required />
         <input className="title" placeholder="Book title" value={form.title} onChange={(e) => titleChange(e)} required />
+        <select className="select" onChange={(e) => catagoryChange(e)} required>
+          <option value="" disabled selected hidden>Please Choose...</option>
+          <option value="Education">Education</option>
+          <option value="Adventure">Adventure</option>
+          <option value="Comedy">Comedy</option>
+          <option value="Drama">Drama</option>
+          <option value="Fantasy">Fantasy</option>
+          <option value="Horror">Horror</option>
+        </select>
         <button className="add_button" type="submit">ADD BOOK</button>
       </form>
     </div>
