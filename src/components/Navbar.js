189b9268e9a556b537f3cdc88ca49ba4eb3e8cb5
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-04-09 21:37:37 +0100

    Implement reviewer's corrections

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ImUser } from 'react-icons/im';
 
 const HeaderStyle = {
-  color: '#b6ad90',
+  color: '#0290ff',
 };
 
 const Navbar = () => (
-  <nav>
-    <h1 style={HeaderStyle}>Bookstore</h1>
-    <ul className="nav_links">
-      <Link to="/"><li>Books</li></Link>
-      <Link to="/Categories"><li>Categories</li></Link>
-    </ul>
+  <nav className="nav">
+    <div className="nav1">
+      <h1 className="header1" style={HeaderStyle}>Bookstore CMS</h1>
+      <ul className="nav_links">
+        <Link to="/"><li>Books</li></Link>
+        <Link to="/Categories"><li className="cate">Categories</li></Link>
+      </ul>
+    </div>
+    <ImUser className="user" />
   </nav>
 );
 
